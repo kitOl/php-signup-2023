b33a0706e2b04071f522cdc5a338b3cf60ce924d
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-04-11 00:44:18 +0300

    add logout code

--- includes/login_model.inc.php
+++ includes/login_model.inc.php
@@ -2,6 +2,15 @@
 
 declare(strict_types = 1);
 
+function output_username()
+{
+    if (isset($_SESSION['user_id'])) {
+        echo "You are logged in as " . $_SESSION['user_username'];
+    } else {
+        echo "You are not logged in";
+    }
+}
+
 function get_user(object $pdo, string $username)
 {
     $query = "SELECT * FROM users WHERE username = :username;";
--- index.php
+++ index.php
@@ -8,37 +8,48 @@ require_once 'includes/login_view.inc.php';
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Sign Up 2023</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/main.css">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP Sign Up 2023</title>
+   <link rel="stylesheet" href="css/reset.css">
+   <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
 
-    <h3>Login</h3>
-    <form action="includes/login.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="pwd" placeholder="Password" autocomplete="off">
-        <button>Login</button>
-    </form>
+   <h3>
+      <?php
+      output_username();
+      ?>
+   </h3>
 
-    <?php
-    check_login_errors();
-    ?>
+   <h3>Login</h3>
+   <form action="includes/login.inc.php" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="pwd" placeholder="Password" autocomplete="off">
+      <button>Login</button>
+   </form>
 
-    <h3>Signup</h3>
-    <form action="includes/signup.inc.php" method="post">
-        <?php
-        signup_inputs();
-        ?>
-        <button>Signup</button>
-    </form>
+   <?php
+   check_login_errors();
+   ?>
 
-    <?php
-    check_signup_errors();
-    ?>
+   <h3>Signup</h3>
+   <form action="includes/signup.inc.php" method="post">
+      <?php
+      signup_inputs();
+      ?>
+      <button>Signup</button>
+   </form>
+
+   <?php
+   check_signup_errors();
+   ?>
+
+   <h3>Logout</h3>
+   <form action="includes/logout.inc.php" method="post">
+      <button>Logout</button>
+   </form>
 
 </body>
 
